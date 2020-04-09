2816b02adb7730ab2d7addf163e9ca9f3195cb48
kundlatsch <gustavo.kundlatsch@gmail.com>
2020-04-09 11:59:29 -0300

    Change reload option name

--- simulation/__main__.py
+++ simulation/__main__.py
@@ -7,7 +7,7 @@ import click
 
 @click.command()
 @click.option("--generate", "-G", default=0, nargs=2, help=strings.generate_help)
-@click.option("--reload-agent/--not-reload", "-R/", default=True, help=strings.reload_help)
+@click.option("--reload/--not-reload", "-R/", default=True, help=strings.reload_help)
 @click.option("--reasoning-time", default=1, help=strings.reasoning_time_help)
 @click.option("--planning-time", default=32, help=strings.planning_time_help)
 def run(generate, reload_agent, reasoning_time, planning_time):
