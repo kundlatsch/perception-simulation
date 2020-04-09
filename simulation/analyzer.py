7551870ff1ae019de7e76be861e2728d88e823bb
kundlatsch <gustavo.kundlatsch@gmail.com>
2020-04-09 18:05:19 -0300

    Create data analyzer

---
+++ simulation/analyzer.py
@@ -0,0 +1,25 @@
+from statistics import mean
+
+def get_mean():
+    results = open('results.txt', 'r')
+
+    content = results.read().split(';')
+    
+    vtimes = []
+    perceptions_processed = []
+    plans_created = []
+
+    for c in content:
+        try:
+            (vtime, perceptions, plans) = c.split(',')
+            vtimes.append(int(vtime))
+            perceptions_processed.append(int(perceptions))
+            plans_created.append(int(plans))
+        except ValueError:
+            pass
+
+    print(vtimes)
+    
+    print(f'VTIMES: {mean(vtimes)}')
+    print(f'PERCEPTIONS PROCESSED: {mean(perceptions_processed)}')
+    print(f'PLANS CREATED: {mean(plans_created)}')
